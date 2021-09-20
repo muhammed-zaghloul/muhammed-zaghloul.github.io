54be7d4ac678e4a4aa3b4514b39fbb8a6da7b9c9
muhammed-zaghloul <70073380+muhammed-zaghloul@users.noreply.github.com>
2021-09-20 03:06:58 +0200

    sheets

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,14 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6e7lrxP2ZAU":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  console.log(datacollect);
 }
 
