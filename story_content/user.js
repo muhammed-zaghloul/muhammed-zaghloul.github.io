83e61f95874ffd180fba17def663384d90d1f49a
muhammed-zaghloul <70073380+muhammed-zaghloul@users.noreply.github.com>
2021-09-20 03:52:30 +0200

    aaaa

--- story_content/user.js
+++ story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6JFQtktpad0":
+      case "5q0JqE5pumU":
         Script1();
         break;
   }
@@ -11,7 +11,24 @@ function ExecuteScript(strId)
 function Script1()
 {
   const player = GetPlayer();
-const datacollect = player.GetVar("datacollect");
-console.log(datacollect);
+const TextEntry = player.GetVar("TextEntry");
+console.log(TextEntry);
+var params = {
+           "range":"Sheet1!"+A+":"+C,
+           "majorDimension": "ROWS",
+           "values": [
+           [TextEntry,"",""]
+          ],
+         }
+         var xhr = new XMLHttpRequest();
+         xhr.open('PUT', 'https://sheets.googleapis.com/v4/spreadsheets/'+"1W2e8PTlT9fVKMWewKKk19t-zlCj7foir3V7RemPuS2U"+'/'+"values/"+"Sheet1!"+A+":"+C+"?"+'valueInputOption=USER_ENTERED');
+         xhr.setRequestHeader('Authorization', 'Bearer ' + "AIzaSyBFEwybAV0rOgM1MDDvAYAegEJa5MCrm74");
+        xhr.onReadyStateChange=function(){
+          console.log(this.status)
+        }
+        xhr.send(JSON.stringify(params));
+
+
+      }
 }
 
