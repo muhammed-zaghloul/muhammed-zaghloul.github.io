8d645526558f9f72cdee7cfb157eca34f983368f
muhammed-zaghloul <70073380+muhammed-zaghloul@users.noreply.github.com>
2021-11-08 00:45:07 +0200

    2lesson

---
+++ ZoomSim/story_content/5tl2dxSCq7W_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.56.26219.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.474%0D%0AAnother%20thing%20you%20can%20permit%20them%20to%20rename%20themselves%20during%20the%20session.%20%0D%0A%0D%0A"}')
