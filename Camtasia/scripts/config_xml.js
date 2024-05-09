ebb8b10b84f727c3f3a9c6546445115736eeb42d
muhammed-zaghloul <muhammed.zaghloul237@gmail.com>
2024-05-09 20:35:02 +0300

    JTC

---
+++ Camtasia/scripts/config_xml.js
@@ -0,0 +1,62 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2024-05-09 08:28:22 م" dc:source="Camtasia,23.2.0,enu" dc:title="Untitled Project" tscDM:firstFrame="Untitled_Project_First_Frame.png" tscDM:originId="5A069CD6-67B5-4816-A974-F051834FCA69" tscDM:project="Untitled Project">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="85066"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="1080" stDim:w="1920"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:label="1" xmpDM:startTime="40330" xmpDM:duration="5000" tscDM:boundingPoly="628,770;1292,770;1292,1022;628,1022;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://drive.google.com/file/d/1kEZBNS5s_sIBb2-UaAK2TUKm0tbWwS7j/view?usp=drive_link" tscHS:newWindow="1"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Introduction" xmpDM:startTime="0" tscDM:image="Untitled_Project_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="عنوان الوحدة" xmpDM:startTime="8800" tscDM:image="Untitled_Project_Thumbnails.png" tscDM:imageindex="1" tscDM:imageoffset="0" tscDM:imagerect="75, 0, 75, 42"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="C01E7DBD-62FE-4EE2-A1AB-9C46BB854E8B" tscIQ:authoredEmail="" tscIQ:requireUserId="0" tscIQ:locale="en-US" tscIQ:reportMethod="NONE" tscIQ:allowSkipQuiz="0" tscIQ:clientId="E7885279-24D1-4E90-AF2A-EF9A7A4E075B" tscIQ:hideReplay="0" tscIQ:quizHash="26770cd204f01d5cf44cbf5ba44c3d6f">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:startTime="71100" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 1"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="0"><tscIQ:question>هذا الفيديو كان مقدمة لشرح الوحدة الاولى من المقرر تحت عنوان القضاء العام؟</tscIQ:question><tscIQ:correctAnswer>2</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>True</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>False</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct"><xmpDM:name><rdf:Alt><rdf:li>أحسنت صنعًا</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect" tscIQ:jumpToTime="8800"><xmpDM:name><rdf:Alt><rdf:li>إجابة خاطئة، تحتاج للرجوع لبداية الفيديو لمعرفة الاجابة الصحيحة</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:startTime="81100" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 3"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="SHORT" tscIQ:id="1"><tscIQ:question>ما رأيك بالمحتوى وكيف يمكننا تطويره</tscIQ:question><tscIQ:correctAnswer/><tscIQ:answerArray/></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tscIQ:QuizParams><rdf:Bag><rdf:li xmpDM:name="txtPrev" xmpDM:value="Previous"/><rdf:li xmpDM:name="txtNext" xmpDM:value="Next"/><rdf:li xmpDM:name="txtAnswerQuestion" xmpDM:value="Take Quiz Now"/><rdf:li xmpDM:name="txtSubmit" xmpDM:value="Submit Answers"/><rdf:li xmpDM:name="txtReview" xmpDM:value="Replay Last Section"/><rdf:li xmpDM:name="txtReviewAnswer" xmpDM:value="View Answers"/><rdf:li xmpDM:name="txtContinue" xmpDM:value="Continue"/></rdf:Bag></tscIQ:QuizParams></rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="Untitled_Project_First_Frame.png"/><rdf:li xmpDM:name="1" xmpDM:value="Untitled_Project_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
